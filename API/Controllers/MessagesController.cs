c83b1289fc84fda34d57726f0eeee03e37e34ff8
Tony Lin <tonylinbusiness@gmail.com>
2020-11-19 14:57:32 -0500

    Added delete message

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -84,7 +84,8 @@ namespace API.Controllers
 
             var message = await _messageRepository.GetMessage(id);
 
-            if(message.Sender.UserName != username && message.Recipient.UserName != username)
+            if(message.Sender.UserName != username 
+                && message.Recipient.UserName != username)
                 return Unauthorized();
 
             if(message.Sender.UserName == username) message.SenderDeleted = true;
