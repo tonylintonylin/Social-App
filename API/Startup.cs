f2e9317af3345969b8a7c2d910abbef936b2e36e
Tony Lin <tonylinbusiness@gmail.com>
2020-11-20 18:31:29 -0500

    Sending messages by hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -77,7 +77,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
-                endpoints.MapHub<MessageHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
